f4e890782a85129d5cb44c68283e47050167692e
Khazanism <khazanyassin@gmail.com>
2022-09-01 23:41:38 +0200

    find ength of a string

---
+++ js_algorithmsDataAndStructures/assets/js/script.js
@@ -0,0 +1,11 @@
+//  js_algorithmsDataAndStructures
+
+
+// ===>Find the length of the string<===
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+console.log(lastNameLength);
