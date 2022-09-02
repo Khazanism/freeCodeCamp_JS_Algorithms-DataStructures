5ac806305928143a2d36ea000da86501666cd6bc
Khazanism <khazanyassin@gmail.com>
2022-09-02 13:07:25 +0200

    Bracket Notation

--- js_algorithmsDataAndStructures/assets/js/script.js
+++ js_algorithmsDataAndStructures/assets/js/script.js
@@ -1,6 +1,7 @@
 //  js_algorithmsDataAndStructures
 
 
+/*
 // ===>Find the length of the string<===
 // Setup
 let lastNameLength = 0;
@@ -9,3 +10,15 @@ const lastName = "Lovelace";
 // Only change code below this line
 lastNameLength = lastName.length;
 console.log(lastNameLength);
+*/
+
+
+// ===>Use Bracket Notation to Find the First Character in a String<===
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+// firstLetterOfLastName = lastName; // Change this line
+firstLetterOfLastName = lastName[0]; // Change this line
