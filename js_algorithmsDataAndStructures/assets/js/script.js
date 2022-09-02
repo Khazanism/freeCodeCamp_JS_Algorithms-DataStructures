efc4a5c45108f46001d9f4288d221628b55c097a
Khazanism <khazanyassin@gmail.com>
2022-09-02 23:51:16 +0200

    String Immutability

--- js_algorithmsDataAndStructures/assets/js/script.js
+++ js_algorithmsDataAndStructures/assets/js/script.js
@@ -1,7 +1,7 @@
 //  js_algorithmsDataAndStructures
 
-
 /*
+
 // ===>Find the length of the string<===
 // Setup
 let lastNameLength = 0;
@@ -10,7 +10,6 @@ const lastName = "Lovelace";
 // Only change code below this line
 lastNameLength = lastName.length;
 console.log(lastNameLength);
-*/
 
 
 // ===>Use Bracket Notation to Find the First Character in a String<===
@@ -22,3 +21,18 @@ const lastName = "Lovelace";
 // Only change code below this line
 // firstLetterOfLastName = lastName; // Change this line
 firstLetterOfLastName = lastName[0]; // Change this line
+
+*/
+
+// ===>Understand String Immutability<===
+
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+//Correct the assignment to myStr so it contains 
+//the string value of Hello World 
+
+// myStr[0] = "H"; // Change this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
