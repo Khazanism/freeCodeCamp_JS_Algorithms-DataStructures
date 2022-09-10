2e1b582890b7a2153a962d1d0e6267afc36af911
Khazanism <khazanyassin@gmail.com>
2022-09-10 21:24:56 +0200

    Manipulate Arrays With unshift()

--- js_algorithmsDataAndStructures/assets/js/script.js
+++ js_algorithmsDataAndStructures/assets/js/script.js
@@ -177,7 +177,6 @@ console.log(removedFromMyArray);
 console.log(myArray);
 
 // Only change code below this line
-*/
 
 // ===> Manipulate Arrays With shift() <===
 
@@ -193,3 +192,19 @@ console.log(myArray);
 console.log(removedFromMyArray);
 
 // Only change code below this line
+*/
+
+// ===> Manipulate Arrays With unshift() <===
+
+// Not only can you shift elements off of the beginning of an array, you can also unshift elements to the beginning of an array i.e. add elements in front of the array.
+
+// .unshift() works exactly like .push(), but instead of adding the element at the end of the array, unshift() adds the element at the beginning of the array.Not only can you shift elements off of the beginning of an array, you can also unshift elements to the beginning of an array i.e. add elements in front of the array.
+
+// .unshift() works exactly like .push(), but instead of adding the element at the end of the array, unshift() adds the element at the beginning of the array.
+
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+// Only change code below this line
+myArray.unshift(["Paul", 35]);
+console.log(myArray);
