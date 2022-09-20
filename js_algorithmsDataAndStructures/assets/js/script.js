997a95abb486b37806226b2ebf171b119293db39
Khazanism <khazanyassin@gmail.com>
2022-09-20 21:13:03 +0200

    Introducing Else Statements

--- js_algorithmsDataAndStructures/assets/js/script.js
+++ js_algorithmsDataAndStructures/assets/js/script.js
@@ -659,19 +659,20 @@ console.log(testLogicalOr(25));
 // When a condition for an if statement is true, the block of code following it is executed. What about when that condition is false? Normally nothing would happen. With an else statement, an alternate block of code can be executed.
 
 function testElse(val) {
-    let result = "";
-    // Only change code below this line
-  
-    if (val > 5) {
-      result = "Bigger than 5";
-    }
-  
-    if (val <= 5) {
-      result = "5 or Smaller";
-    }
-  
-    // Only change code above this line
-    return result;
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
   }
-  
-  console.log(testElse(4));
+
+  // Only change code above this line
+  return result;
+}
+
+console.log(testElse(4));
+console.log(testElse(5));
+console.log(testElse(6));
+console.log(testElse(10));
