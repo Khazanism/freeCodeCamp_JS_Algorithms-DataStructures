9cf1ee9e3be821fe892c2809f738419864b5cdb3
Khazanism <khazanyassin@gmail.com>
2022-09-24 16:52:29 +0200

    Replacing If Else Chains with Switch

--- js_algorithmsDataAndStructures/assets/js/script.js
+++ js_algorithmsDataAndStructures/assets/js/script.js
@@ -858,7 +858,6 @@ console.log(switchOfStuff("b"));
 console.log(switchOfStuff("c"));
 console.log(switchOfStuff("d"));
 console.log(switchOfStuff(4));
-*/
 
 // ===> Multiple Identical Options in Switch Statements <===
 
@@ -900,3 +899,52 @@ console.log(sequentialSizes(6));
 console.log(sequentialSizes(7));
 console.log(sequentialSizes(8));
 console.log(sequentialSizes(9));
+*/
+
+// ===> Replacing If Else Chains with Switch <===
+
+// If you have many options to choose from, a switch statement can be easier to write than many chained if/else if statements. The following:
+
+// if (val === 1) {
+//   answer = "a";
+// } else if (val === 2) {
+//   answer = "b";
+// } else {
+//   answer = "c";
+// } ===> EXDPLANATION
+
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+
+    default:
+      answer = "";
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+console.log(chainToSwitch("bob"));
+console.log(chainToSwitch(42));
+console.log(chainToSwitch(1));
+console.log(chainToSwitch(99));
+console.log(chainToSwitch(7));
+console.log(chainToSwitch("John"));
+console.log(chainToSwitch(156));
