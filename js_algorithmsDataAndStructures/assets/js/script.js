5927ca35f009d5e2ea5dbc057b07a844a6996e9c
Khazanism <khazanyassin@gmail.com>
2022-10-03 21:00:32 +0200

    Accessing Object Properties with Bracket Notation

--- js_algorithmsDataAndStructures/assets/js/script.js
+++ js_algorithmsDataAndStructures/assets/js/script.js
@@ -1098,7 +1098,6 @@ const myDog = {
   "friends": ["mimi", "modi"]
   // Only change code above this line
 };
-*/
 
 // ===> Accessing Object Properties with Dot Notation <==
 
@@ -1116,4 +1115,22 @@ const testObj = {
   // Only change code below this line
   const hatValue = testObj.hat;      // Change this line
   const shirtValue = testObj.shirt;    // Change this line
+  */
+  
+  // ===> Accessing Object Properties with Bracket Notation <==
+
+//   The second way to access the properties of an object is bracket notation ([]). If the property of the object you are trying to access has a space in its name, you will need to use bracket notation.
+
+// However, you can still use bracket notation on object properties without spaces.
+  // Setup
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  // Only change code below this line
+  const entreeValue = testObj ["an entree"];   // Change this line
+  const drinkValue = testObj ["the drink"];    // Change this line
+
   
