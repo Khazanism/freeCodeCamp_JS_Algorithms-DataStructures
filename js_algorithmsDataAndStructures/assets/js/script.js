54ca56b31f1c61daf28c1600f754ef50d86ec6c7
Khazanism <khazanyassin@gmail.com>
2022-10-03 21:14:05 +0200

    Updating Object Properties

--- js_algorithmsDataAndStructures/assets/js/script.js
+++ js_algorithmsDataAndStructures/assets/js/script.js
@@ -1132,22 +1132,38 @@ const testObj = {
   const entreeValue = testObj ["an entree"];   // Change this line
   const drinkValue = testObj ["the drink"];    // Change this line
   
-  */
- 
- // ===> Accessing Object Properties with Variables <==
-
-//  Another use of bracket notation on objects is to access a property which is stored as the value of a variable. This can be very useful for iterating through an object's properties or when accessing a lookup table.
-
-// Setup
+  
+  // ===> Accessing Object Properties with Variables <==
+  
+  //  Another use of bracket notation on objects is to access a property which is stored as the value of a variable. This can be very useful for iterating through an object's properties or when accessing a lookup table.
+  
+  // Setup
 const testObj = {
     12: "Namath",
     16: "Montana",
     19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj [playerNumber];   // Change this line
+console.log(player);
+*/
+
+// ===>Updating Object Properties<==
+
+// After you've created a JavaScript object, you can update its properties at any time just like you would update any other variable. You can use either dot or bracket notation to update
+
+// Setup
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
   };
   
   // Only change code below this line
-  const playerNumber = 16;  // Change this line
-  const player = testObj [playerNumber];   // Change this line
-  console.log(player);
+  myDog ["name"] = "happy Coder";
+  
 
   
