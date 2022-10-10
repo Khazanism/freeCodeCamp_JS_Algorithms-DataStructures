4a2b6aeb09c84bee9d81172f1f0ce8051670f898
Khazanism <khazanyassin@gmail.com>
2022-10-10 20:07:12 +0200

    Record Collection

--- js_algorithmsDataAndStructures/assets/js/script.js
+++ js_algorithmsDataAndStructures/assets/js/script.js
@@ -1325,23 +1325,68 @@ const myStorage = {
 const gloveBoxContents = myStorage.car.inside["glove box"];
 
 console.log(gloveBoxContents);
-*/
 
 // ===> Accessing Nested Arrays <==
 
 // As we have seen in earlier examples, objects can contain both nested objects and nested arrays. Similar to accessing nested objects, array bracket notation can be chained to access nested arrays.
 
 const myPlants = [
-  {
-    type: "flowers",
-    list: ["rose", "tulip", "dandelion"],
-  },
-  {
-    type: "trees",
-    list: ["fir", "pine", "birch"],
-  },
+    {
+        type: "flowers",
+        list: ["rose", "tulip", "dandelion"],
+    },
+    {
+        type: "trees",
+        list: ["fir", "pine", "birch"],
+    },
 ];
 
 const secondTree = myPlants[1].list[1];
 
 console.log(secondTree);
+*/
+
+// ===> Record Collection <==
+
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: "Slippery When Wet",
+    artist: "Bon Jovi",
+    tracks: ["Let It Rock", "You Give Love a Bad Name"],
+  },
+  2468: {
+    albumTitle: "1999",
+    artist: "Prince",
+    tracks: ["1999", "Little Red Corvette"],
+  },
+  1245: {
+    artist: "Robert Palmer",
+    tracks: [],
+  },
+  5439: {
+    albumTitle: "ABBA Gold",
+  },
+};
+
+// Only change code below this line
+if (prop !== "tracks" && value !== "") {
+  records[id][prop] = value;
+} else if (prop == "tracks" ) {
+
+}
+function updateRecords(records, id, prop, value) {
+  return records;
+}
+
+console.log(updateRecords(recordCollection, 5439, "artist", "ABBA"));
+console.log(
+  updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me")
+);
+console.log(updateRecords(recordCollection, 2548, "artist", ""));
+console.log(
+  updateRecords(recordCollection, 1245, "tracks", "Addicted to Love")
+);
+console.log(updateRecords(recordCollection, 2468, "tracks", "Free"));
+console.log(updateRecords(recordCollection, 2548, "tracks", ""));
+console.log(updateRecords(recordCollection, 1245, "albumTitle", "Riptide"));
