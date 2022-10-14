aecd601ce04700958fdfb790e329f444cff1018a
Khazanism <khazanyassin@gmail.com>
2022-10-14 21:11:08 +0200

    Iterate with JavaScript While Loops

--- js_algorithmsDataAndStructures/assets/js/script.js
+++ js_algorithmsDataAndStructures/assets/js/script.js
@@ -1344,66 +1344,80 @@ const myPlants = [
 const secondTree = myPlants[1].list[1];
 
 console.log(secondTree);
-*/
 
 // ===> Record Collection <==
 
 // Setup
 const recordCollection = {
-  2548: {
-    albumTitle: "Slippery When Wet",
-    artist: "Bon Jovi",
-    tracks: ["Let It Rock", "You Give Love a Bad Name"],
-  },
-  2468: {
+    2548: {
+        albumTitle: "Slippery When Wet",
+        artist: "Bon Jovi",
+        tracks: ["Let It Rock", "You Give Love a Bad Name"],
+    },
+    2468: {
     albumTitle: "1999",
     artist: "Prince",
     tracks: ["1999", "Little Red Corvette"],
-  },
-  1245: {
+},
+1245: {
     artist: "Robert Palmer",
     tracks: [],
-  },
-  5439: {
-    albumTitle: "ABBA Gold",
+},
+5439: {
+      albumTitle: "ABBA Gold",
   },
 };
 
 // Only change code below this line
 
 function updateRecords(records, id, prop, value) {
-  if (prop !== "tracks" && value !== "") {
-    records[id][prop] = value;
-  } else if (prop === "tracks" && records[id][prop] == undefined) {
-    records[id][prop] = [value];
-  } else if (prop === "tracks" && value !== "") {
+    if (prop !== "tracks" && value !== "") {
+        records[id][prop] = value;
+    } else if (prop === "tracks" && records[id][prop] == undefined) {
+        records[id][prop] = [value];
+    } else if (prop === "tracks" && value !== "") {
     records[id][prop].push(value);
-  } else if (value === "") {
+} else if (value === "") {
     delete records[id][prop];
-  }
-  return records;
+}
+return records;
 }
 
 console.log(
-  JSON.stringify(updateRecords(recordCollection, 5439, "artist", "ABBA"))
+    JSON.stringify(updateRecords(recordCollection, 5439, "artist", "ABBA"))
+    );
+    console.log(
+        updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me")
+        );
+        console.log(
+            JSON.stringify(updateRecords(recordCollection, 2548, "artist", ""))
+            );
+            console.log(
+                JSON.stringify(
+                    updateRecords(recordCollection, 1245, "tracks", "Addicted to Love")
+        )
+        );
+        console.log(
+            JSON.stringify(updateRecords(recordCollection, 2468, "tracks", "Free"))
 );
 console.log(
-  updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me")
-);
-console.log(
-  JSON.stringify(updateRecords(recordCollection, 2548, "artist", ""))
-);
-console.log(
-  JSON.stringify(
-    updateRecords(recordCollection, 1245, "tracks", "Addicted to Love")
-  )
-);
-console.log(
-  JSON.stringify(updateRecords(recordCollection, 2468, "tracks", "Free"))
-);
-console.log(
-  JSON.stringify(updateRecords(recordCollection, 2548, "tracks", ""))
-);
-console.log(
-  JSON.stringify(updateRecords(recordCollection, 1245, "albumTitle", "Riptide"))
-);
+    JSON.stringify(updateRecords(recordCollection, 2548, "tracks", ""))
+    );
+    console.log(
+        JSON.stringify(updateRecords(recordCollection, 1245, "albumTitle", "Riptide"))
+        );
+        
+        */
+// ===> Iterate with JavaScript While Loops <==
+
+// The first type of loop we will learn is called a while loop because it runs while a specified condition is true and stops once that condition is no longer true.
+// Setup
+const myArray = [];
+
+// Only change code below this line
+let i = 5;
+while (i >= 0) {
+  myArray.push(i);
+  i--;
+}
+console.log(myArray);
