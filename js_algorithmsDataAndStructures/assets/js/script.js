d89136311cdfb3f00624854e5c99db06d0ba7864
Khazanism <khazanyassin@gmail.com>
2022-10-14 20:45:31 +0200

    record collection 05

--- js_algorithmsDataAndStructures/assets/js/script.js
+++ js_algorithmsDataAndStructures/assets/js/script.js
@@ -1373,11 +1373,14 @@ const recordCollection = {
 function updateRecords(records, id, prop, value) {
   if (prop !== "tracks" && value !== "") {
     records[id][prop] = value;
-  } else if (prop === "tracks" && records[id][prop] === undefined) {
+  } else if (prop == "tracks" && records[id][prop] == undefined) {
     records[id][prop] = [value];
-  } else {
-    return records;
+  } else if (prop === "traks") {
+    records[id][prop].push(value);
+  } else if (value == "") {
+    delete records[id][prop];
   }
+  return records;
 }
 
 console.log(
