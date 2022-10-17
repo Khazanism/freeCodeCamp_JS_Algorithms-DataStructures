5e4840cb7355ee0a5e0b209b7e1c96989c1e06eb
Khazanism <khazanyassin@gmail.com>
2022-10-17 23:41:59 +0200

    Iterate Through an Array with a For Loop

--- js_algorithmsDataAndStructures/assets/js/script.js
+++ js_algorithmsDataAndStructures/assets/js/script.js
@@ -1461,7 +1461,6 @@ for (let i = 1; i < 10; i += 2) {
 
 console.log(myArray);
 
-*/
 
 // ===> Count Backwards With a For Loop <===
 
@@ -1475,11 +1474,26 @@ const myArray = [];
 // Only change code below this line
 
 for ( let i = 9; i > 0; i -= 2) {
- myArray.push(i) ;
+    myArray.push(i) ;
 };
 
 console.log(myArray);
+*/
 
+// ===> Iterate Through an Array with a For Loop <===
+
+// A common task in JavaScript is to iterate through the contents of an array. One way to do that is with a for loop. This code will output each element of the array arr to the console:
+
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+
+// Only change code below this line
+let total = 0;
+for (let i = 0; i < myArr.length; i++) {
+  total += myArr[i];
+};
+
+console.log(total);
 
 
 
