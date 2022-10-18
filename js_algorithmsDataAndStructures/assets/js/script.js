acf4bd7cd1b3666cb7c6d79e4df6e60ee10bf8da
Khazanism <khazanyassin@gmail.com>
2022-10-18 23:41:02 +0200

    Profile Lookup

--- js_algorithmsDataAndStructures/assets/js/script.js
+++ js_algorithmsDataAndStructures/assets/js/script.js
@@ -1547,20 +1547,19 @@ do {
 
 console.log(myArray);
 console.log([i]);
-*/
 
 // ===> Replace Loops using Recursion <===
 
 // Recursion is the concept that a function can be expressed in terms of itself.
 
 function sum(arr, n) {
-  // Only change code below this line
-  if (n <= 0) {
-    return 0;
-  } else {
-    return sum(arr, n - 1) + arr[n - 1];
-  }
-  // Only change code above this line
+    // Only change code below this line
+    if (n <= 0) {
+        return 0;
+    } else {
+        return sum(arr, n - 1) + arr[n - 1];
+    }
+    // Only change code above this line
 }
 
 console.log(sum([1], 0));
@@ -1568,3 +1567,41 @@ console.log(sum([1], 0));
 console.log(sum([2, 3, 4], 1));
 
 console.log(sum([2, 3, 4, 5], 3));
+*/
+
+// ===> Profile Lookup <===
+
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  // Only change code above this line
+}
+
+lookUpProfile("Akira", "likes");
