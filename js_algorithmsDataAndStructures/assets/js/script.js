ee096fc708a6ad1f0b2a64a88f1891adca5407fc
Khazanism <khazanyassin@gmail.com>
2022-10-18 19:24:35 +0200

    Iterate with JavaScript Do...While Loops

--- js_algorithmsDataAndStructures/assets/js/script.js
+++ js_algorithmsDataAndStructures/assets/js/script.js
@@ -1493,37 +1493,58 @@ for (let i = 0; i < myArr.length; i++) {
 };
 
 console.log(total);
-*/
 
 // ===> Nesting For Loops <===
 
 // If you have a multi-dimensional array, you can use the same logic as the prior waypoint to loop through both the array and any sub-arrays.
 
 function multiplyAll(arr) {
-  let product = 1;
+    let product = 1;
   // Only change code below this line
-
+  
   for (var i = 0; i < arr.length; i++) {
     for (var j = 0; j < arr[i].length; j++) {
       product *= arr[i][j];
     }
-  }
-  // Only change code above this line
-  return product;
+}
+// Only change code above this line
+return product;
 }
 
 console.log(multiplyAll([[1], [2], [3]]));
 console.log(
-  multiplyAll([
-    [1, 2],
-    [3, 4],
+    multiplyAll([
+        [1, 2],
+        [3, 4],
     [5, 6, 7],
-  ])
+])
 );
 console.log(
-  multiplyAll([
-    [5, 1],
+    multiplyAll([
+        [5, 1],
     [0.2, 4, 0.5],
     [3, 9],
-  ])
+])
 );
+*/
+
+// ===> Iterate with JavaScript Do...While Loops <===
+
+// It is called a do...while loop because it will first do one pass of the code inside the loop no matter what, and then continue to run the loop while the specified condition evaluates to true.
+
+// Setup
+const myArray = [];
+let i = 10;
+
+// Only change code below this line
+do {
+  myArray.push(i);
+  i++;
+} while (i < 5);
+{
+  // myArray.push(i);
+  // i++;
+}
+
+console.log(myArray);
+console.log([i]);
