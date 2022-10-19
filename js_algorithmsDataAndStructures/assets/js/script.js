1b9eee420376ee3895ccc41b1d0eafce98429528
Khazanism <khazanyassin@gmail.com>
2022-10-19 12:47:45 +0200

    Profile Lookup 01

--- js_algorithmsDataAndStructures/assets/js/script.js
+++ js_algorithmsDataAndStructures/assets/js/script.js
@@ -1601,7 +1601,23 @@ const contacts = [
 
 function lookUpProfile(name, prop) {
   // Only change code below this line
+  for (i = 0; i < contacts.length; i++) {
+    if (contacts[i].firstName === name) {
+      if (contacts[i].hasOwnProperty(prop)) {
+        return contacts[i][prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact";
   // Only change code above this line
 }
+console.log(lookUpProfile("Kristian", "lastName"));
+console.log(lookUpProfile("Sherlock", "likes"));
+console.log(lookUpProfile("Harry", "likes"));
+console.log(lookUpProfile("Bob", "number"));
+console.log(lookUpProfile("Bob", "potato"));
+console.log(lookUpProfile("Akira", "address"));
 
-lookUpProfile("Akira", "likes");
+// lookUpProfile("Akira", "likes");
