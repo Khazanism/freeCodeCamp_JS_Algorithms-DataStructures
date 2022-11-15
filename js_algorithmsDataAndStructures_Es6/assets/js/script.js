63df33cadac27d4c92bfa67e97fae766370cff8f
Khazanism <khazanyassin@gmail.com>
2022-11-15 23:09:48 +0100

    Use getters and setters to Control Access to an Object

--- js_algorithmsDataAndStructures_Es6/assets/js/script.js
+++ js_algorithmsDataAndStructures_Es6/assets/js/script.js
@@ -385,42 +385,14 @@ class Thermostat {
   }
 
   // getter
-  get temp() {
+  get temperature() {
     return (5 / 9) * (this.fahrenheit - 32);
   }
   // setter
-  set temp(celsius) {
-    this.farenheit = (celsius * 9.0) / 5 + 32;
-  }
-
-};
-
-// Only change code above this line
-
-const thermos = new Thermostat(76); // Setting in Fahrenheit scale
-let temp = thermos.temperature; // 24.44 in Celsius
-thermos.temperature = 26;
-temp = thermos.temperature; // 26 in Celsius
-
-console.log(Thermostat(76));
-
-// Only change code below this line
-
-class Thermostat {
-  constructor(fahrenheit) {
-    this.fahrenheit = fahrenheit;
-  }
-
-  // getter
-  get temp() {
-    return (5 / 9) * (this.fahrenheit - 32);
-  }
-  // setter
-  set temp(celsius) {
+  set temperature(celsius) {
     this.fahrenheit = (celsius * 9.0) / 5 + 32;
   }
-
-};
+}
 
 // Only change code above this line
 
@@ -429,4 +401,3 @@ let temp = thermos.temperature; // 24.44 in Celsius
 thermos.temperature = 26;
 temp = thermos.temperature; // 26 in Celsius
 
-// console.log(Thermostat(76));
