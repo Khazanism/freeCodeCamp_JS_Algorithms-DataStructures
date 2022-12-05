f50b9b2cfb8b5b6e5f3faafc3ec55383fbf46501
Khazanism <khazanyassin@gmail.com>
2022-12-05 23:39:35 +0100

    Use typeof to Check the Type of a Variabl

--- js_algorithmsDataAndStructures_Debugging/assets/js/script.js
+++ js_algorithmsDataAndStructures_Debugging/assets/js/script.js
@@ -9,9 +9,20 @@ a++;
 console.log(a);
 let sumAB = a + b;
 console.log(sumAB);
-*/
 
 // ---> Understanding the Differences between the freeCodeCamp and Browser Console <---
 let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
 console.log(output);
 console.clear();
+*/
+
+// ---> Use typeof to Check the Type of a Variabl <---
+
+// JavaScript recognizes seven primitive (immutable) data types: Boolean, Null, Undefined, Number, String, Symbol (new with ES6), and BigInt (new with ES2020), and one type for mutable items: Object. Note that in JavaScript, arrays are technically a type of object.
+
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+console.log(typeof seven);
+console.log(typeof three);
