4444ae4a159f7b9d854a5b03db80f2a1de0ff298
Khazanism <khazanyassin@gmail.com>
2022-12-09 20:36:41 +0100

    Access an Array's Contents Using Bracket Notation

--- js_algorithmsDataAndStructures_DataStructure/assets/js/script.js
+++ js_algorithmsDataAndStructures_DataStructure/assets/js/script.js
@@ -1,6 +1,14 @@
 //  js_algorithmsDataAndStructures
 
 /*
- */
 // ---> Use an Array to Store a Collection of Data <---
 let yourArray = ["a", 1, undefined, 50, true]; // Change this line
+console.log(yourArray.length);
+*/
+// ---> Access an Array's Contents Using Bracket Notation <---
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = "A7o";
+// Only change code above this line
+console.log(myArray);
