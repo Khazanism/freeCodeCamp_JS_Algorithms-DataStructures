891330c5e36ef9d65672f2c9e7683c813e3e1984
Khazanism <khazanyassin@gmail.com>
2022-12-09 21:04:18 +0100

    Remove Items from an Array with pop() and shift()

--- js_algorithmsDataAndStructures_DataStructure/assets/js/script.js
+++ js_algorithmsDataAndStructures_DataStructure/assets/js/script.js
@@ -11,15 +11,29 @@ let myArray = ["a", "b", "c", "d"];
 myArray[1] = "A7o";
 // Only change code above this line
 console.log(myArray);
-*/
 
 // ---> Add Items to an Array with push() and unshift() <---
 
 function mixedNumbers(arr) {
-  // Only change code below this line
-  arr.push(7, "VIII", 9);
-  arr.unshift("I", 2, "three");
-  // Only change code above this line
-  return arr;
+    // Only change code below this line
+    arr.push(7, "VIII", 9);
+    arr.unshift("I", 2, "three");
+    // Only change code above this line
+    return arr;
 }
 console.log(mixedNumbers(["IV", 5, "six"]));
+*/
+
+// ---> Remove Items from an Array with pop() and shift() <---
+function popShift(arr) {
+    let popped = arr.pop(); // Change this line
+    let shifted = arr.shift(); // Change this line
+    return [shifted, popped];
+  }
+  
+  console.log(popShift(['challenge', 'is', 'not', 'complete']));
+  
+
+
+
+
