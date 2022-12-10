47e52f205acdfd59195bf9334a5183e82772b1f9
Khazanism <khazanyassin@gmail.com>
2022-12-10 15:51:36 +0100

    Copy Array Items Using slice()

--- js_algorithmsDataAndStructures_DataStructure/assets/js/script.js
+++ js_algorithmsDataAndStructures_DataStructure/assets/js/script.js
@@ -39,7 +39,6 @@ const arr = [2, 4, 5, 1, 7, 5, 2, 1];
 arr.splice(1, 4);
 // Only change code above this line
 console.log(arr);
-*/
 
 // ---> Add Items Using splice() <---
 
@@ -51,11 +50,25 @@ function htmlColorNames(arr) {
 }
 
 console.log(
-  htmlColorNames([
-    "DarkGoldenRod",
-    "WhiteSmoke",
-    "LavenderBlush",
-    "PaleTurquoise",
-    "FireBrick",
-  ])
-);
+    htmlColorNames([
+        "DarkGoldenRod",
+        "WhiteSmoke",
+        "LavenderBlush",
+        "PaleTurquoise",
+        "FireBrick",
+    ])
+    );
+*/
+
+// ---> Copy Array Items Using slice() <---
+
+function forecast(arr) {
+    // Only change code below this line
+//   arr.splice(0, 2); // answer 1
+//   arr.splice(2, 4); // answer 1
+//   let forecast = arr .slice(0,1); // answer 1
+    return arr.slice(2, 4); // answer 2
+  }
+  
+  // Only change code above this line
+  console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
