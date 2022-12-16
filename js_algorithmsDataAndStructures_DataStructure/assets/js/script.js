c72658687d298b49fb0b4f34f646fb477fbecb2d
Khazanism <khazanyassin@gmail.com>
2022-12-16 20:41:50 +0100

    Use the delete Keyword to Remove Object Properties

--- js_algorithmsDataAndStructures_DataStructure/assets/js/script.js
+++ js_algorithmsDataAndStructures_DataStructure/assets/js/script.js
@@ -210,7 +210,6 @@ userActivity.data.online = 45
 // Only change code above this line
 
 console.log(userActivity);
-*/
 
 // ---> Access Property Names with Bracket Notation <---
 
@@ -230,3 +229,23 @@ function checkInventory(scannedItem) {
 }
 
 console.log(checkInventory("apples"));
+*/
+
+// ---> Use the delete Keyword to Remove Object Properties <---
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27,
+};
+
+// Only change code below this line
+delete foods.oranges;
+delete foods.plums;
+delete foods.strawberries;
+// Only change code above this line
+
+console.log(foods);
