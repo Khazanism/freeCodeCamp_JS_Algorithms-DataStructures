c9c9f36ac6a8073c79523f2ea0fd6d32ae44cdc3
Khazanism <khazanyassin@gmail.com>
2022-12-17 20:43:47 +0100

    Modify an Array Stored in an Object

--- js_algorithmsDataAndStructures_DataStructure/assets/js/script.js
+++ js_algorithmsDataAndStructures_DataStructure/assets/js/script.js
@@ -316,7 +316,6 @@ function countOnline(usersObj) {
 }
 
 console.log(countOnline(users));
-*/
 
 // ---> Generate an Array of All Object Keys with Object.keys() <---
 
@@ -340,9 +339,34 @@ let users = {
 };
 function getArrayOfUsers(obj) {
   // Only change code below this line
-
+  
   return Object.keys(obj)
   // Only change code above this line
 }
 
 console.log(getArrayOfUsers(users));
+*/
+
+// ---> Modify an Array Stored in an Object <---
+let user = {
+  name: "Kenneth",
+  age: 28,
+  data: {
+    username: "kennethCodesAllDay",
+    joinDate: "March 26, 2016",
+    organization: "freeCodeCamp",
+    friends: ["Sam", "Kira", "Tomo"],
+    location: {
+      city: "San Francisco",
+      state: "CA",
+      country: "USA",
+    },
+  },
+};
+
+function addFriend(userObj, friend) {
+  // Only change code below this line
+  // Only change code above this line
+}
+
+console.log(addFriend(user, "Pete"));
