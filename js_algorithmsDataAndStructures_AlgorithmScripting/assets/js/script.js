9f20ede3f772320dc8a0a80373a83125c2e1e9b9
Khazanism <khazanyassin@gmail.com>
2022-12-19 16:41:51 +0100

    Convert Celsius to Fahrenheit

---
+++ js_algorithmsDataAndStructures_AlgorithmScripting/assets/js/script.js
@@ -0,0 +1,16 @@
+//  js_algorithmsDataAndStructures
+
+/*
+ */
+// ---> Convert Celsius to Fahrenheit <---
+function convertCtoF(celsius) {
+  let fahrenheit;
+  fahrenheit = celsius * (9 / 5) + 32;
+  return fahrenheit;
+}
+
+console.log(convertCtoF(0));
+console.log(convertCtoF(-30));
+console.log(convertCtoF(-10));
+console.log(convertCtoF(20));
+console.log(convertCtoF(30));
