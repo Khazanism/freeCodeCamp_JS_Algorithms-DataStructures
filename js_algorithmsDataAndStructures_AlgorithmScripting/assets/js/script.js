73667d73c160e0ab186011022af0f3bde6a5aad8
Khazanism <khazanyassin@gmail.com>
2022-12-19 17:28:55 +0100

    Reverse a String

--- js_algorithmsDataAndStructures_AlgorithmScripting/assets/js/script.js
+++ js_algorithmsDataAndStructures_AlgorithmScripting/assets/js/script.js
@@ -14,3 +14,14 @@ console.log(convertCtoF(-30));
 console.log(convertCtoF(-10));
 console.log(convertCtoF(20));
 console.log(convertCtoF(30));
+
+// ---> Reverse a String <---
+
+function reverseString(str) {
+  let array = str.split("").reverse().join("");
+  return array;
+}
+
+console.log(reverseString("hello"));
+console.log(reverseString("Howdy"));
+console.log(reverseString("Greetings from Earth"));
