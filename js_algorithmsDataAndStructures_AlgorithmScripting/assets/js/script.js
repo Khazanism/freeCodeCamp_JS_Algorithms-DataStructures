0d883004c9cd1d7c7c82187c4d85473bd5aa37f3
Khazanism <khazanyassin@gmail.com>
2022-12-20 19:58:14 +0100

    Find the Longest Word in a String

--- js_algorithmsDataAndStructures_AlgorithmScripting/assets/js/script.js
+++ js_algorithmsDataAndStructures_AlgorithmScripting/assets/js/script.js
@@ -24,7 +24,6 @@ function reverseString(str) {
 console.log(reverseString("hello"));
 console.log(reverseString("Howdy"));
 console.log(reverseString("Greetings from Earth"));
-*/
 
 // ---> Factorialize a Number <---
 function factorialize(num) {
@@ -39,3 +38,38 @@ console.log(factorialize(5));
 console.log(factorialize(10));
 console.log(factorialize(20));
 console.log(factorialize(5));
+*/
+
+// ---> Find the Longest Word in a String <---
+
+function findLongestWordLength(str) {
+  let array = str.split(" ");
+  if (array.length == 1) {
+    return array[0].length;
+  }
+
+  return Math.max(
+    array[0].length,
+    findLongestWordLength(array.slice(1).join(" "))
+  );
+}
+
+console.log(
+  findLongestWordLength("The quick brown fox jumped over the lazy dog")
+);
+
+console.log(findLongestWordLength("May the force be with you"));
+
+console.log(findLongestWordLength("Google do a barrel roll"));
+
+console.log(
+  findLongestWordLength(
+    "What is the average airspeed velocity of an unladen swallow"
+  )
+);
+
+console.log(
+  findLongestWordLength(
+    "What if we try a super-long word such as otorhinolaryngology"
+  )
+);
