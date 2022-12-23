e60881608ebe6733640ca80efedc5836201fec0a
Khazanism <khazanyassin@gmail.com>
2022-12-23 23:57:37 +0100

    Confirm the Ending

--- js_algorithmsDataAndStructures_AlgorithmScripting/assets/js/script.js
+++ js_algorithmsDataAndStructures_AlgorithmScripting/assets/js/script.js
@@ -124,7 +124,6 @@ function findLongestWordLength(s) {
 function findLongestWordLength(str) {
   return Math.max(...str.split(" ").map((word) => word.length));
 }
-*/
 
 // ---> Return Largest Numbers in Arrays <---
 
@@ -152,23 +151,31 @@ console.log(
     [32, 35, 37, 39],
     [1000, 1001, 857, 1],
   ])
-);
-
-console.log(
-  largestOfFour([
-    [4, 9, 1, 3],
-    [13, 35, 18, 26],
-    [32, 35, 97, 39],
-    [1000000, 1001, 857, 1],
-  ])
-);
-
-console.log(
-  largestOfFour([
-    [17, 23, 25, 12],
-    [25, 7, 34, 48],
-    [4, -10, 18, 21],
-    [-72, -3, -17, -10],
-  ])
-);
-
+  );
+  
+  console.log(
+    largestOfFour([
+      [4, 9, 1, 3],
+      [13, 35, 18, 26],
+      [32, 35, 97, 39],
+      [1000000, 1001, 857, 1],
+    ])
+    );
+    
+    console.log(
+      largestOfFour([
+        [17, 23, 25, 12],
+        [25, 7, 34, 48],
+        [4, -10, 18, 21],
+        [-72, -3, -17, -10],
+      ])
+      );
+      */
+      
+      // ---> Confirm the Ending <---
+      
+      function confirmEnding(str, target) {
+        return str;
+      }
+      
+      confirmEnding("Bastian", "n");
