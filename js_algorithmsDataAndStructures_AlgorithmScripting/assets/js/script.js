e6fb109c1acc56cbf1fd73a3b5ccdf02c0f0c7ca
Khazanism <khazanyassin@gmail.com>
2022-12-29 19:53:23 +0100

    Slice and Splice--> 1st Answer

--- js_algorithmsDataAndStructures_AlgorithmScripting/assets/js/script.js
+++ js_algorithmsDataAndStructures_AlgorithmScripting/assets/js/script.js
@@ -335,14 +335,26 @@ console.log(titleCase("I'm a little tea pot"));
 
 function frankenSplice(arr1, arr2, n) {
   console.log(arr2.splice(n, ...arr2));
-
+  
   const elementOfFirst = arr1.slice();
   // console.log(elementOfFirst);
   // console.log(arr2[n]);
   const elementOfSecond = arr2.splice(n, ...arr2);
   console.log([elementOfSecond, elementOfFirst]);
-
+  
   // return arr2;
 }
 
 console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+
+// --------------------> 1st answer
+
+function frankenSplice(arr1, arr2, n) {
+  // It's alive. It's alive!
+  let localArray = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    localArray.splice(n, 0, arr1[i]);
+    n++;
+  }
+  return localArray;
+}
