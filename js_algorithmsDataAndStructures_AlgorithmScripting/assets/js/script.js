cd574ad426632fb173f2e8ecd0cf5b48c5097c70
Khazanism <khazanyassin@gmail.com>
2022-12-29 19:54:36 +0100

    Slice and Splice--> 2nd Answer

--- js_algorithmsDataAndStructures_AlgorithmScripting/assets/js/script.js
+++ js_algorithmsDataAndStructures_AlgorithmScripting/assets/js/script.js
@@ -358,3 +358,12 @@ function frankenSplice(arr1, arr2, n) {
   }
   return localArray;
 }
+
+// --------------------> 2nd answer
+
+function frankenSplice(arr1, arr2, n) {
+  // It's alive. It's alive!
+  let localArr = arr2.slice();
+  localArr.splice(n, 0, ...arr1);
+  return localArr;
+}
