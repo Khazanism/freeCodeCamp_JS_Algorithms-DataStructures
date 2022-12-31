d9035f060d5af68efd8b5d1b04dbcb4f5e2b53a7
Khazanism <khazanyassin@gmail.com>
2022-12-31 13:31:51 +0100

    Caesars Cipher // Project 5

---
+++ js_algorithmsDataAndStructures_Projects/assets/js/Caesars Cipher_Script .js
@@ -0,0 +1,21 @@
+//  js_algorithmsDataAndStructures
+
+// ---> Caesars Cipher <---
+
+function rot13(str) {
+  let codeArr = [];
+  for (let i = 0; i < str.length; i++) {
+    codeArr.push(str.charCodeAt(i));
+  }
+  let decodeArr = codeArr.map(function (code) {
+    if (code >= 65 && code <= 77) {
+      return code + 13;
+    } else if (code >= 78 && code <= 90) {
+      return code - 13;
+    } else if (code < 65) {
+      return code;
+    }
+  });
+  return String.fromCharCode(...decodeArr);
+}
+rot13("SERR PBQR PNZC");
