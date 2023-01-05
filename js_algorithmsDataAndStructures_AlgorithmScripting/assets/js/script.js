66efff64be1a27a0a8c761db67121dd5b71a5f8d
Khazanism <khazanyassin@gmail.com>
2023-01-05 19:38:11 +0100

    Basic Algorithm Scripting: Chunky Monkey --> 2nd Answer

--- js_algorithmsDataAndStructures_AlgorithmScripting/assets/js/script.js
+++ js_algorithmsDataAndStructures_AlgorithmScripting/assets/js/script.js
@@ -487,3 +487,12 @@ console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3));
 console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2));
 
 console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4));
+
+// --------------------> 3rd answer
+function chunkArrayInGroups(arr, size) {
+  var newArray = [];
+  while (arr.length) {
+    newArray.push(arr.splice(0, size));
+  }
+  return newArray;
+}
