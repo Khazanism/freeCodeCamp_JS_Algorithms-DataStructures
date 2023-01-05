8fcef180a4d8e72b81defd03b9ff385bbaf2816f
Khazanism <khazanyassin@gmail.com>
2023-01-05 20:25:51 +0100

    Use Dot Notation to Access the Properties of an Object

--- js_algorithmsDataAndStructures_ObjectOrientedProgramming/assets/js/script.js
+++ js_algorithmsDataAndStructures_ObjectOrientedProgramming/assets/js/script.js
@@ -1,6 +1,6 @@
 //  js_algorithmsDataAndStructures
 
-// /*
+/*
 // ---> Object Oriented Programming <---
 
 // ---> Create a Basic JavaScript Object <---
@@ -8,3 +8,13 @@ let dog = {
   name: "Ahbal",
   numLegs: 2
 };
+*/
+// ---> Use Dot Notation to Access the Properties of an Object <---
+
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+};
+// Only change code below this line
+console.log(dog.name);
+console.log(dog.numLegs);
