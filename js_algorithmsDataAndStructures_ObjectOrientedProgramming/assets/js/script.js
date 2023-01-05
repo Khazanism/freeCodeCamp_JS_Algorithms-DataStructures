0fc4bfb43db7c41f1dc0de73d91abb5e1dcd4b22
Khazanism <khazanyassin@gmail.com>
2023-01-05 20:37:43 +0100

    Make Code More Reusable with the this Keyword

--- js_algorithmsDataAndStructures_ObjectOrientedProgramming/assets/js/script.js
+++ js_algorithmsDataAndStructures_ObjectOrientedProgramming/assets/js/script.js
@@ -17,7 +17,6 @@ let dog = {
 // Only change code below this line
 console.log(dog.name);
 console.log(dog.numLegs);
-*/
 
 // ---> Create a Method on an Object <---
 let dog = {
@@ -25,6 +24,18 @@ let dog = {
   numLegs: 4,
   sayLegs: function () {
     return "This dog has " + dog.numLegs + " legs.";
+  },
+};
+
+console.log(dog.sayLegs());
+*/
+
+// ---> Make Code More Reusable with the this Keyword <---
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function () {
+    return "This dog has " + this.numLegs + " legs.";
   },
 };
 
