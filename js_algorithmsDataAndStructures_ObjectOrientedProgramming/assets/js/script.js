068fb4f64e9e1655a59455beb6d99f53885d332c
Khazanism <khazanyassin@gmail.com>
2023-01-05 20:22:15 +0100

    Create a Basic JavaScript Object

--- js_algorithmsDataAndStructures_ObjectOrientedProgramming/assets/js/script.js
+++ js_algorithmsDataAndStructures_ObjectOrientedProgramming/assets/js/script.js
@@ -1,5 +1,10 @@
 //  js_algorithmsDataAndStructures
 
-/*
+// /*
 // ---> Object Oriented Programming <---
-*/
+
+// ---> Create a Basic JavaScript Object <---
+let dog = {
+  name: "Ahbal",
+  numLegs: 2
+};
