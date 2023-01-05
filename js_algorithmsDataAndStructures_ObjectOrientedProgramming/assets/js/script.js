7e28f3e14813e0f23756936a3ade4ac8042ea50b
Khazanism <khazanyassin@gmail.com>
2023-01-05 20:34:38 +0100

    Create a Method on an Object

--- js_algorithmsDataAndStructures_ObjectOrientedProgramming/assets/js/script.js
+++ js_algorithmsDataAndStructures_ObjectOrientedProgramming/assets/js/script.js
@@ -8,7 +8,6 @@ let dog = {
   name: "Ahbal",
   numLegs: 2
 };
-*/
 // ---> Use Dot Notation to Access the Properties of an Object <---
 
 let dog = {
@@ -18,3 +17,15 @@ let dog = {
 // Only change code below this line
 console.log(dog.name);
 console.log(dog.numLegs);
+*/
+
+// ---> Create a Method on an Object <---
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function () {
+    return "This dog has " + dog.numLegs + " legs.";
+  },
+};
+
+console.log(dog.sayLegs());
