2ab46ae76daeb7887dd8893c5c7d9879c7d2df7d
Khazanism <khazanyassin@gmail.com>
2023-01-07 19:32:57 +0100

    Use a Mixin to Add Common Behavior Between Unrelated Objects

--- js_algorithmsDataAndStructures_ObjectOrientedProgramming/assets/js/script.js
+++ js_algorithmsDataAndStructures_ObjectOrientedProgramming/assets/js/script.js
@@ -349,7 +349,6 @@ let beagle = new Dog();
 
 beagle.bark();
 beagle.eat();
-*/
 
 // ---> Override Inherited Methods <---
 
@@ -373,3 +372,29 @@ Penguin.prototype.fly = function () {
 
 let penguin = new Penguin();
 console.log(penguin.fly());
+*/
+
+// ---> Use a Mixin to Add Common Behavior Between Unrelated Objects <---
+
+let bird = {
+  name: "Donald",
+  numLegs: 2,
+};
+
+let boat = {
+  name: "Warrior",
+  type: "race-boat",
+};
+
+// Only change code below this line
+
+let glideMixin = function (obj) {
+  obj.glide = function () {
+    return "HObAAAaa";
+  };
+};
+
+glideMixin(bird);
+glideMixin(boat);
+console.log(bird.glide());
+console.log(boat.glide());
