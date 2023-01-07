2575ce9fcdb7ef66e9470d3636ef14fe51e0becb
Khazanism <khazanyassin@gmail.com>
2023-01-07 19:44:36 +0100

    Use Closure to Protect Properties Within an Object from Being Modified Externally

--- js_algorithmsDataAndStructures_ObjectOrientedProgramming/assets/js/script.js
+++ js_algorithmsDataAndStructures_ObjectOrientedProgramming/assets/js/script.js
@@ -372,7 +372,6 @@ Penguin.prototype.fly = function () {
 
 let penguin = new Penguin();
 console.log(penguin.fly());
-*/
 
 // ---> Use a Mixin to Add Common Behavior Between Unrelated Objects <---
 
@@ -398,3 +397,35 @@ glideMixin(bird);
 glideMixin(boat);
 console.log(bird.glide());
 console.log(boat.glide());
+*/
+
+// ---> Use a Mixin to Add Common Behavior Between Unrelated Objects <---
+
+// Think about things like passwords and bank accounts being easily changeable by any part of your codebase. That could cause a lot of issues.
+
+// The simplest way to make this public property private is by creating a variable within the constructor function. This changes the scope of that variable to be within the constructor function versus available globally. This way, the variable can only be accessed and changed by methods also within the constructor function.
+
+// -------> important
+// function Bird() {
+//   let hatchedEgg = 10;
+
+//   this.getHatchedEggCount = function() {
+//     return hatchedEgg;
+//   };
+// }
+// let ducky = new Bird();
+// ducky.getHatchedEggCount();
+// -------> important
+
+// Here getHatchedEggCount is a privileged method, because it has access to the private variable hatchedEgg. This is possible because hatchedEgg is declared in the same context as getHatchedEggCount. In JavaScript, a function always has access to the context in which it was created. This is called closure.
+
+function Bird() {
+  let weight = 15;
+  this.getWeight = function () {
+    return weight;
+  };
+}
+
+let duck = new Bird();
+duck.getWeight();
+console.log(duck.getWeight());
