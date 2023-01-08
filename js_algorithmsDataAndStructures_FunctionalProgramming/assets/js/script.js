e299340d084c8db063681fa7cc4ae9976227de27
Khazanism <khazanyassin@gmail.com>
2023-01-08 15:54:57 +0100

    Learn About Functional Programming

--- js_algorithmsDataAndStructures_FunctionalProgramming/assets/js/script.js
+++ js_algorithmsDataAndStructures_FunctionalProgramming/assets/js/script.js
@@ -1,6 +1,34 @@
 //  js_algorithmsDataAndStructures
 
-/*
+// /*
 // ---> Functional Programming <---
 
-// ---> Functional Programming <---
+// ---> Learn About Functional Programming <---
+
+// Functional programming is about:
+// Isolated functions - there is no dependence on the state of the program, which includes global variables that are subject to change
+// Pure functions - the same input always gives the same output
+// Functions with limited side effects - any changes, or mutations, to the state of the program outside the function are carefully controlled
+
+// Function that returns a string representing a cup of green tea
+const prepareTea = () => "greenTea";
+console.log(prepareTea());
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (numOfCups) => {
+  const teaCups = [];
+
+  for (let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+// console.log(teaCups);
+// Only change code below this line
+const tea4TeamFCC = getTea(40);
+console.log(tea4TeamFCC);
+// Only change code above this line
