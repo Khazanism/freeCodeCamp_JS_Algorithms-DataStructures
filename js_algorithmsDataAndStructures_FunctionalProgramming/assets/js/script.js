0f61a9b1d163d57342da71659e7c921c7b653ed1
Khazanism <khazanyassin@gmail.com>
2023-01-09 23:40:53 +0100

    Refactor Global Variables Out of Functions

--- js_algorithmsDataAndStructures_FunctionalProgramming/assets/js/script.js
+++ js_algorithmsDataAndStructures_FunctionalProgramming/assets/js/script.js
@@ -194,3 +194,36 @@ function remove(bookList, bookName) {
 // console.log(remove(bookList, "On The Electrodynamics of Moving Bodies"));
 
 // console.log(remove(add(bookList, "A Brief History of Time"), "On The Electrodynamics of Moving Bodies"));
+
+// The global variable
+const bookList = [
+  "The Hound of the Baskervilles",
+  "On The Electrodynamics of Moving Bodies",
+  "Philosophiæ Naturalis Principia Mathematica",
+  "Disquisitiones Arithmeticae",
+];
+
+// Change code below this line
+function add(bookList, bookName) {
+  bookList.push(bookName);
+  return bookList;
+
+  // Change code above this line
+}
+
+// Change code below this line
+function remove(bookList, bookName) {
+  const book_index = bookList.indexOf(bookName);
+  if (book_index >= 0) {
+    bookList.splice(book_index, 1);
+    return bookList;
+
+    // Change code above this line
+  }
+}
+
+// console.log(add(bookList, "A Brief History of Time"));
+
+// console.log(remove(bookList, "On The Electrodynamics of Moving Bodies"));
+
+// console.log(remove(add(bookList, "A Brief History of Time"), "On The Electrodynamics of Moving Bodies"));
