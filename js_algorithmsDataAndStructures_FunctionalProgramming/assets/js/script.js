e6e1c15b556135207d7f49aa1179c41e17977c94
Khazanism <khazanyassin@gmail.com>
2023-01-12 23:24:32 +0100

    Implement map on a Prototype--->1st Answer

--- js_algorithmsDataAndStructures_FunctionalProgramming/assets/js/script.js
+++ js_algorithmsDataAndStructures_FunctionalProgramming/assets/js/script.js
@@ -342,11 +342,16 @@ console.log(JSON.stringify(ratings));
 */
 
 // ---> Implement map on a Prototype <---
+
 Array.prototype.myMap = function (callback) {
   const newArray = [];
   // Only change code below this line
 
-  // Only change code above this line
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(callback(this[i], i, this));
+  }
+
+  // Only change co de above this line
   return newArray;
 };
 
