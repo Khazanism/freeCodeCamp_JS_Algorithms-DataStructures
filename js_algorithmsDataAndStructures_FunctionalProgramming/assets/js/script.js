0940593cbab9bad813d941442351d84e3d446567
Khazanism <khazanyassin@gmail.com>
2023-01-13 17:56:57 +0100

    Implement map on a Prototype

--- js_algorithmsDataAndStructures_FunctionalProgramming/assets/js/script.js
+++ js_algorithmsDataAndStructures_FunctionalProgramming/assets/js/script.js
@@ -348,19 +348,24 @@ Array.prototype.myMap = function (callback) {
   // Only change code below this line
 
   for (let i = 0; i < this.length; i++) {
+    // callback(this[i], i, this)
+    // console.log(newArray.push(callback(this[i], i, this)))
     newArray.push(callback(this[i], i, this));
+    // newArray.push(callback(this[i]))
   }
 
   // Only change co de above this line
   return newArray;
 };
 
-console.log([23, 65, 98, 5, 13].myMap((item) => item * 2));
+var newArr = [23, 65, 98, 5, 13].myMap((item) => item * 2);
 
-console.log(
-  ["naomi", "quincy", "camperbot"].myMap((element) => element.toUpperCase())
+var newArr = ["naomi", "quincy", "camperbot"].myMap((element) =>
+  element.toUpperCase()
 );
 
-console.log(
-  [1, 1, 2, 5, 2].myMap((element, index, array) => array[index + 1] || array[0])
+var newArr = [1, 1, 2, 5, 2].myMap(
+  (element, index, array) => array[index + 1] || array[0]
 );
+
+console.log(newArr);
