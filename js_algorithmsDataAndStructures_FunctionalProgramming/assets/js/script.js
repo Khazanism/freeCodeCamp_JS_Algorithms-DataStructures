3dbe8581bc637bd519a60c1398cbd5c1a4119c5a
Khazanism <khazanyassin@gmail.com>
2023-01-21 17:45:11 +0100

    Return a Sorted Array Without Changing the Original Array--> 1st Opt

--- js_algorithmsDataAndStructures_FunctionalProgramming/assets/js/script.js
+++ js_algorithmsDataAndStructures_FunctionalProgramming/assets/js/script.js
@@ -791,11 +791,12 @@ function alphabeticalOrder(arr) {
 
 // ---> Return a Sorted Array Without Changing the Original Array <---
 
+// ----------> 1st Opt
 const globalArray = [5, 6, 3, 2, 9];
 
 function nonMutatingSort(arr) {
   // Only change code below this line
-  return arr.sort(function (a, b) {
+  return arr.slice().sort(function (a, b) {
     return a === b ? 0 : a < b ? -1 : 1;
   });
   // Only change code above this line
