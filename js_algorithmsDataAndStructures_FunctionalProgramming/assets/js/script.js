ea9b687e01280b41dc4dd48c977b1a59e2bff81f
Khazanism <khazanyassin@gmail.com>
2023-01-21 19:03:36 +0100

    Combine an Array into a String Using the join Method

--- js_algorithmsDataAndStructures_FunctionalProgramming/assets/js/script.js
+++ js_algorithmsDataAndStructures_FunctionalProgramming/assets/js/script.js
@@ -840,7 +840,13 @@ console.log(splitify("This.is.a-sentence"));
 
 function sentensify(str) {
   // Only change code below this line
+  return str.split(/\W/).join(" ");
+
   // Only change code above this line
 }
 
-sentensify("May-the-force-be-with-you");
+console.log(sentensify("May-the-force-be-with-you"));
+
+console.log(sentensify("The.force.is.strong.with.this.one"));
+
+console.log(sentensify("There,has,been,an,awakening"));
