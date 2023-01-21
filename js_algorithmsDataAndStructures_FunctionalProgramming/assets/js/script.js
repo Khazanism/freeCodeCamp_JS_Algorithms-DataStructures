3ef274f57b9d50a17522b0b59dd7324f55e182c1
Khazanism <khazanyassin@gmail.com>
2023-01-21 15:41:34 +0100

    Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem

--- js_algorithmsDataAndStructures_FunctionalProgramming/assets/js/script.js
+++ js_algorithmsDataAndStructures_FunctionalProgramming/assets/js/script.js
@@ -750,10 +750,15 @@ console.log(getRating(watchList));
 // ---> Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem <---
 const squareList = (arr) => {
   // Only change code below this line
-  return arr.filter((num) => num > 1).filter((num) => Number.isInteger(num));
+  return arr
+    .filter((num) => num > 1)
+    .filter((num) => Number.isInteger(num))
+    .map((num) => num * num);
   // Only change code above this line
 };
 
 const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+const squaredIntegers = squareList([-3.7, -5, 3, 10, 12.5, 7, -4.5, -17, 0.3]);
 const squaredIntegers = squareList([4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2]);
+
 console.log(squaredIntegers);
