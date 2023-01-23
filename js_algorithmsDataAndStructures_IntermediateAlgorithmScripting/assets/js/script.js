2e95973c4d87297215f195538cb9a04fbd52650d
Khazanism <khazanyassin@gmail.com>
2023-01-23 23:53:18 +0100

    Sum All Numbers in a Range-->2nd Opt

--- js_algorithmsDataAndStructures_IntermediateAlgorithmScripting/assets/js/script.js
+++ js_algorithmsDataAndStructures_IntermediateAlgorithmScripting/assets/js/script.js
@@ -4,6 +4,7 @@
  */
 // ---> Intermediate Algorithm Scripting <---
 // ---> Sum All Numbers in a Range <---
+
 // ----------> 1st Opt
 function sumAll(arr) {
   let min, max;
@@ -29,3 +30,18 @@ console.log(sumAll([4, 1]));
 console.log(sumAll([5, 10]));
 console.log(sumAll([10, 5]));
 console.log(sumAll([1, 0]));
+
+// ----------> 2nd Opt
+
+function sumAll(arr) {
+  const min = arr[0] < arr[1] ? arr[0] : arr[1];
+  const max = arr[0] > arr[1] ? arr[0] : arr[1];
+
+  let sum = 0;
+
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
