401d2b85b7af32e588594266e0fcd307a252a843
Khazanism <khazanyassin@gmail.com>
2023-01-25 20:02:02 +0100

    Diff Two Arrays-->1st Opt

--- js_algorithmsDataAndStructures_IntermediateAlgorithmScripting/assets/js/script.js
+++ js_algorithmsDataAndStructures_IntermediateAlgorithmScripting/assets/js/script.js
@@ -58,11 +58,40 @@ function sumAll(arr) {
 }
 */
 
-// ---> Sum All Numbers in a Range <---
+// ---> Diff Two Arrays <---
 
+// ----------> 1st Opt
 function diffArray(arr1, arr2) {
   const newArr = [];
-  return newArr;
+  
+  for (let i = 0; i < arr1.length; i++) {
+    if (!newArr.includes(arr1[i])) {
+      newArr.push(arr1[i]);
+    }
+  }
+  
+  for (let i = 0; i < arr2.length; i++) {
+    if (!newArr.includes(arr2[i])) {
+      newArr.push(arr2[i]);
+    }
+  }
+
+  const symmDif = [];
+  for (let i = 0; i < newArr.length; i++) {
+    const currentEl = newArr[i];
+    if (arr1.includes(currentEl) && !arr2.includes(currentEl)) {
+      symmDif.push(currentEl);
+    } else if (arr2.includes(currentEl) && !arr1.includes(currentEl)) {
+      symmDif.push(currentEl);
+    }
+  }
+  return symmDif;
 }
 
-diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+
+console.log(diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]));
+
+console.log(diffArray([], ["snuffleupagus", "cookie monster", "elmo"]));
+
+console.log(diffArray([1, "calf", 3, "piglet"], [7, "filly"]));
