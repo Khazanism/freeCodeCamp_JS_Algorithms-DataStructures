c99011d0af4aa38601722a846a5ac0556d472b51
Khazanism <khazanyassin@gmail.com>
2023-01-25 20:00:38 +0100

    Diff Two Arrays --> 1st Opt

--- js_algorithmsDataAndStructures_IntermediateAlgorithmScripting/assets/js/script.js
+++ js_algorithmsDataAndStructures_IntermediateAlgorithmScripting/assets/js/script.js
@@ -1,7 +1,6 @@
 //  js_algorithmsDataAndStructures
 
 /*
- */
 // ---> Intermediate Algorithm Scripting <---
 // ---> Sum All Numbers in a Range <---
 
@@ -15,13 +14,13 @@ function sumAll(arr) {
     max = arr[1];
     min = arr[0];
   }
-
+  
   let sum = 0;
-
+  
   for (let i = min; i <= max; i++) {
     sum += i;
   }
-
+  
   return sum;
 }
 
@@ -36,13 +35,13 @@ console.log(sumAll([1, 0]));
 function sumAll(arr) {
   const min = arr[0] < arr[1] ? arr[0] : arr[1];
   const max = arr[0] > arr[1] ? arr[0] : arr[1];
-
+  
   let sum = 0;
-
+  
   for (let i = min; i <= max; i++) {
     sum += i;
   }
-
+  
   return sum;
 }
 
@@ -50,10 +49,20 @@ function sumAll(arr) {
 
 function sumAll(arr) {
   let sum = 0;
-
+  
   for (let i = Math.min(...arr); i <= Math.max(...arr); i++) {
     sum += i;
   }
-
+  
   return sum;
 }
+*/
+
+// ---> Sum All Numbers in a Range <---
+
+function diffArray(arr1, arr2) {
+  const newArr = [];
+  return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
