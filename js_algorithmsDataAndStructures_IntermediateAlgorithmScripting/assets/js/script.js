29d26847e41be39e9a8c981efa9f79aeff565e3d
Khazanism <khazanyassin@gmail.com>
2023-01-28 20:59:07 +0100

    Wherefore art thou-->3rd Opt

--- js_algorithmsDataAndStructures_IntermediateAlgorithmScripting/assets/js/script.js
+++ js_algorithmsDataAndStructures_IntermediateAlgorithmScripting/assets/js/script.js
@@ -256,3 +256,28 @@ let x = whatIsInAName(
 );
 
 console.log(x);
+
+// ----------> 2nd Opt
+
+function whatIsInAName(collection, source) {
+  const keys = Object.keys(source);
+  return collection.filter((obj) => {
+    return keys.reduce((acc, key) => {
+      if (obj[key] !== source[key]) {
+        return false;
+      }
+      return acc;
+    }, true);
+  });
+}
+
+let x = whatIsInAName(
+  [
+    { first: "Romeo", last: "Montague" },
+    { first: "Mercutio", last: null },
+    { first: "Tybalt", last: "Capulet" },
+  ],
+  { last: "Capulet" }
+);
+
+console.log(x);
