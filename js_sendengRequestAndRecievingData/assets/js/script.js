90a7b817ba9ffe51b64250db4b8bb6d37574201b
Khazanism <khazanyassin@gmail.com>
2023-02-24 16:52:08 +0100

    Fetching Data // main structure

--- js_sendengRequestAndRecievingData/assets/js/script.js
+++ js_sendengRequestAndRecievingData/assets/js/script.js
@@ -7,11 +7,12 @@
 // any data i request ---> is recieved as array of objects 20 <---> 11:20.
 
 // -----> Xhttp Request <-----
-
+//1st METHOD
 let xhttp = new XMLHttpRequest();
 
 xhttp.onload = function () {
-  console.log(xhttp.responseText); // but this returns string of objects
+  //   console.log(xhttp.responseText); // but this returns string of objects
+  console.log(JSON.parse(xhttp.responseText));
 };
 
 xhttp.open("GET", "https://jsonplaceholder.typicode.com/users"); // key open we use it to recieve  or "POST"
@@ -19,4 +20,16 @@ xhttp.open("GET", "https://jsonplaceholder.typicode.com/users"); // key open we 
 xhttp.send(); // send request to ge t users
 
 // json is data that looks like normal objects but all keys are in douple quotes " data "
-// --------------> but we want to convert this string to array ?!
+// ------------> but we want to convert this string to array by using ---> JSON.parse
+
+//2nd METHOD
+
+// promise [resolve "if the data came"- reject "if th data didn;t came"]
+
+// fetch--> is a type of Promise
+
+fetch("https://jsonplaceholder.typicode.com/users");
+// wriring the resolve
+.then()
+// writting the rejection
+.catch()
