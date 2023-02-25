7ac5cefedfd32481750f7b1b5049008428b911b8
Khazanism <khazanyassin@gmail.com>
2023-02-25 17:09:41 +0100

    Best Practise for Fetching Data

--- js_sendengRequestAndRecievingData/assets/js/script.js
+++ js_sendengRequestAndRecievingData/assets/js/script.js
@@ -24,22 +24,57 @@ xhttp.send(); // send request to ge t users
 //2nd METHOD
 
 // promise [resolve "if the data came"- reject "if th data didn;t came"]
-*/
 
 // fetch--> is a type of Promise
 
 fetch("https://jsonplaceholder.typicode.com/users")
 // wriring the resolve
 .then(function (test) {
-    test.json() // it holds the info (object) <---its a peomise by itself so we write then and catch again
-    .then(function (data) {
-      console.log(data);
-    })
-
-    .catch();
+  test.json() // it holds the info (object) <---its a peomise by itself so we write then and catch again
+  .then(function (data) {
+    console.log(data);
+  })
+  
+  .catch();
 })
 // writting the rejection
 .catch(function () {
-    
+  
 });
 // IMPORTANT we write ; at the end of the last catch 
+*/
+
+//3rd METHOD <-----> shorter Method
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   // wriring the resolve
+//   .then(function (res) {
+//     return res.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   })
+//   .catch(function (err) {
+//     console.log("please check main code");
+//   });
+
+// Arrow function
+
+// its preferees that the fetch in not floating in the page and to include it in in a function.
+
+function fetchUsers() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => {        // will be removed in react
+      return res.json();  // will be removed in react
+    })                    // will be removed in react
+    .then(data => {
+      for (let i = 0; i < data.length; i++) {
+        console.log(data[i].email);
+      }
+    })
+    .catch(err => {
+      console.log("please check main code");
+    });
+}
+
+fetchUsers(); // when we need to operate it --->  we just call the function <---
