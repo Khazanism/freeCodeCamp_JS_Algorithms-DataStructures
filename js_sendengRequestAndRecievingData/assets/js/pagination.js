adf2d11d64c82b1a49fb8480774ba4f72adc08e8
Khazanism <khazanyassin@gmail.com>
2023-02-28 23:09:14 +0100

    pagination example

---
+++ js_sendengRequestAndRecievingData/assets/js/pagination.js
@@ -0,0 +1,17 @@
+let products = [
+  "prod1",
+  "prod2",
+  "prod3",
+  "prod4",
+  "prod5",
+  "prod6",
+  "prod7",
+  "prod8",
+  "prod9",
+  "prod10",
+];
+
+// Num of Product 10
+
+let productsPerPage = 2;
+let pages = products.length / productsPerPage;
